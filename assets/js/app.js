c618335a784b2e536408ee39be9232be31f7973e
suzon-ahmmed <info.suzonahamed@gmail.com>
2022-03-31 23:49:03 +0600

    modify filter input field padding

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,26 @@
-
-
-
 /**
- * sidebar Module
+ * bootstrap tooltip Module
  * 
  * @param void
  * @return void
  */
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+   return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+/**
+ * see mor function of flight ticket Module
+ * 
+ * @param void
+ * @return void
+ */
+ function seeMore() {
+   const showMoreBtn = document.querySelector(".flight-show-more-btn");
+   const showMoreUi = document.querySelector(".details-content .right-details ul");
+   showMoreBtn.addEventListener("click", () => {
+      //Toggle Nav
+      showMoreUi.classList.toggle("scrole-ui");
+   });
+ }
+ seeMore();
